a362042d5a2e38347e598357a5ff768687184f4d
Daniil <danechka.serebrov@mail.ru>
2024-04-17 23:49:04 +0300

    work_10.2

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,31 @@
 <template>
   <div class="notes">
-    <div class="titleOfNotes has-text-centered">
+    <div class="titleOfNotes has-text-centered animationTitleOfNotes">
       <h1>My notes</h1>
     </div>
 
-    <div class="field has-addons-centered has-addons">
-      <div class="control">
-        <input class="input" type="text" placeholder="Find a repository" />
+    <form class="mb-5" @submit.prevent="addNotes">
+      <div class="field has-addons-centered has-addons">
+        <div class="control">
+          <input v-model="newNotesCont" class="input has-background-dark" type="text" placeholder="Find a repository" />
+        </div>
+        <div class="control">
+          <button :disabled="!newNotesCont" class="button is-info">Search</button>
+        </div>
       </div>
-      <div class="control">
-        <button class="button is-info">Search</button>
-      </div>
-    </div>
+    </form>
 
-    <div v-for="list in lists" :key="list.id" class="card mb-5">
+    <div v-for="list in lists" :key="list.id" class="card mb-5 has-background-dark animationCard">
       <div class="card-content">
         <div class="content">
           <div class="columns is-mobile is-vcentered">
             <div class="column is-left">
-              <div class="lineBreaks">
-                <div>&#8470;{{ list.number }}</div>
-                <div class="mt-2">
-                  {{ list.content }}
-                </div>
-              </div>
+              {{ list.content }}
             </div>
             <div class="column is-2 is-text-right">
               <div class="lineBreaksBtn">
                 <div>
-                  <button class="button is-black">&#10004;</button>
+                  <button class="button is-info">&#10004;</button>
                 </div>
                 <div>
                   <button class="button is-danger mt-2">&#9932;</button>
@@ -44,33 +41,44 @@
 
 <script setup>
 import { ref } from "vue";
+import { v4 as uuidv4 } from 'uuid';
 
 const lists = ref([
-  {
-    id: "id1",
-    number: "1",
-    content: "lorem",
-    done: true,
-  },
-  {
-    id: "id2",
-    number: "2",
-    content: "content2",
-    done: true,
-  },
-  {
-    id: "id3",
-    number: "3",
-    content: "content3",
-    done: true,
-  },
-  {
-    id: "id4",
-    number: "4",
-    content: "content4",
-    done: true,
-  },
+  // {
+  //   id: "id1",
+  //   content: "lorem",
+  //   done: true,
+  // },
+  // {
+  //   id: "id2",
+  //   content: "content2",
+  //   done: true,
+  // },
+  // {
+  //   id: "id3",
+  //   content: "content3",
+  //   done: true,
+  // },
+  // {
+  //   id: "id4",
+  //   content: "content4",
+  //   done: true,
+  // },
 ]);
+
+const addNotes = () => {
+  const newNotes = {
+    id: uuidv4(),
+    content: newNotesCont.value,
+    done: true,
+  };
+  lists.value.unshift(newNotes);
+  newNotesCont.value = '';
+};
+
+const newNotesCont = ref("");
+
+
 </script>
 
 <style>
@@ -86,14 +94,86 @@ const lists = ref([
   padding: 10px;
 }
 
-.lineBreaks {
-  display: flex;
-  flex-direction: column;
-}
-
 .lineBreaksBtn {
   display: flex;
   flex-direction: column;
   align-items: center;
 }
+.animationTitleOfNotes{
+  -webkit-animation: text-focus-in 1s cubic-bezier(0.550, 0.085, 0.680, 0.530) both;
+	        animation: text-focus-in 1s cubic-bezier(0.550, 0.085, 0.680, 0.530) both;
+}
+
+
+.animationCard{
+  -webkit-animation: slide-in-blurred-top 0.6s cubic-bezier(0.230, 1.000, 0.320, 1.000) both;
+	        animation: slide-in-blurred-top 0.6s cubic-bezier(0.230, 1.000, 0.320, 1.000) both;
+}
+
+ @-webkit-keyframes text-focus-in {
+  0% {
+    -webkit-filter: blur(12px);
+            filter: blur(12px);
+    opacity: 0;
+  }
+  100% {
+    -webkit-filter: blur(0px);
+            filter: blur(0px);
+    opacity: 1;
+  }
+}
+@keyframes text-focus-in {
+  0% {
+    -webkit-filter: blur(12px);
+            filter: blur(12px);
+    opacity: 0;
+  }
+  100% {
+    -webkit-filter: blur(0px);
+            filter: blur(0px);
+    opacity: 1;
+  }
+}
+
+ @-webkit-keyframes slide-in-blurred-top {
+  0% {
+    -webkit-transform: translateY(-1000px) scaleY(2.5) scaleX(0.2);
+            transform: translateY(-1000px) scaleY(2.5) scaleX(0.2);
+    -webkit-transform-origin: 50% 0%;
+            transform-origin: 50% 0%;
+    -webkit-filter: blur(40px);
+            filter: blur(40px);
+    opacity: 0;
+  }
+  100% {
+    -webkit-transform: translateY(0) scaleY(1) scaleX(1);
+            transform: translateY(0) scaleY(1) scaleX(1);
+    -webkit-transform-origin: 50% 50%;
+            transform-origin: 50% 50%;
+    -webkit-filter: blur(0);
+            filter: blur(0);
+    opacity: 1;
+  }
+}
+@keyframes slide-in-blurred-top {
+  0% {
+    -webkit-transform: translateY(-1000px) scaleY(2.5) scaleX(0.2);
+            transform: translateY(-1000px) scaleY(2.5) scaleX(0.2);
+    -webkit-transform-origin: 50% 0%;
+            transform-origin: 50% 0%;
+    -webkit-filter: blur(40px);
+            filter: blur(40px);
+    opacity: 0;
+  }
+  100% {
+    -webkit-transform: translateY(0) scaleY(1) scaleX(1);
+            transform: translateY(0) scaleY(1) scaleX(1);
+    -webkit-transform-origin: 50% 50%;
+            transform-origin: 50% 50%;
+    -webkit-filter: blur(0);
+            filter: blur(0);
+    opacity: 1;
+  }
+}
+
 </style>
