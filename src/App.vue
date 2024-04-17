8f4cf48317f683e78249bb51b67a0a829b1a1784
Daniil727 <danechka.serebrov@mail.ru>
2024-04-17 14:40:37 +0300

    Intermediate: adding card

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,29 @@
       <h1>My notes</h1>
     </div>
 
-    <div class="field has-addons">
+    <div class="field has-addons-centered has-addons">
       <div class="control">
         <input class="input" type="text" placeholder="Find a repository" />
       </div>
       <div class="control">
         <button class="button is-info">Search</button>
+      </div>
+    </div>
+
+    <div v-for="i in 3" :key="i" class="card mb-5">
+      <div class="card-content">
+        <div class="content">
+          <div class="columns is-mobile is-vcentered">
+            <div class="column is-left">
+              Lorem ipsum leo Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. 
+            </div>
+            <div class="column is-4 is-text-right ">
+              <button class="button is-black">&#128504;</button>
+              <button class="button is-danger ml-2">&#9932;</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -22,10 +39,10 @@
 
 .notes {
   margin: 0 auto;
-  max-width: 800px;
-  padding: 20px;
+  max-width: 500px;
+  padding: 10px;
 }
 .titleOfNotes {
-  padding: 20px;
+  padding: 10px;
 }
 </style>
