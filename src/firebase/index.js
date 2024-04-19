33b056efce5b7529bcc59b874203f784491084e3
Daniil727 <danechka.serebrov@mail.ru>
2024-04-19 08:29:23 +0300

    install_firebase

---
+++ src/firebase/index.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCuYexJ4CYiOXwuvSujAUpQoHIkd8XGa44",
+  authDomain: "vue-js-composition-api.firebaseapp.com",
+  projectId: "vue-js-composition-api",
+  storageBucket: "vue-js-composition-api.appspot.com",
+  messagingSenderId: "123986718556",
+  appId: "1:123986718556:web:e5e2ce787a3b1cd8925b99"
+};
+
+const app = initializeApp(firebaseConfig);
