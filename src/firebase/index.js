eb867e1970fce4fa0a86dcf1650bee9ae135f8cb
Daniil727 <danechka.serebrov@mail.ru>
2024-04-19 15:25:45 +0300

    Firebase

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCuYexJ4CYiOXwuvSujAUpQoHIkd8XGa44",
@@ -10,3 +11,8 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export {
+    db
+}
